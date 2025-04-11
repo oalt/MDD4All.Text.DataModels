d3279ee675a60f6b5ccced4cf1b6184ad78a5522
oalt <oa@lacheweg21.de>
2025-04-11 17:40:38 +0200

    Initial code commit

---
+++ src/MDD4All.Text.DataModels/FontDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace MDD4All.Text.DataModels
+{
+    public class FontDescription
+    {
+        public FontFamily FontFamily { get; set; }
+        
+        public FontStyle FontStyle { get; set; }
+
+        public FontWeight FontWeight { get; set; }
+
+        public int FontSize { get; set; }
+    }
+}
---
+++ src/MDD4All.Text.DataModels/FontWeight.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace MDD4All.Text.DataModels
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum FontWeight
+    {
+        Normal,
+        Bold
+    }
+}
